1e86af9f238601900f0c53a0630445dc2f8087e4
devinerickson15 <devinjohn15@gmail.com>
2018-04-06 10:27:06 -0400

    MVC example code

---
+++ CIT360 Java/src/MVC/MapView.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MVC;
+
+
+
+/**
+ *
+ * @author JacobsonKe
+ */
+public class MapView {
+    
+    public MapView() {                          
+    }
+    
+    public void displayMap(){
+        Game game = LostInSpace.getCurrentGame();
+        ModelMap map = game.getMap();
+        Planet[] location = map.getLocation();
+        
+        for(int i=0; i < location.length; i++){
+            System.out.println(
+            "Planet " + location[i].getName() + ": "
+            + location[i].getDesc());
+        }
+    }
+}
